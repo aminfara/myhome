88775c69294922e43825552c00fe416d91264344
Ali Aminfar <ali.aminfar@gmail.com>
2019-05-26 01:39:56 +1000

    Added support for n node manager

--- .config/fish/conf.d/03-my_fish_paths.fish
+++ .config/fish/conf.d/03-my_fish_paths.fish
@@ -1,3 +1,8 @@
 if test -d $fzf_dir/bin
     set -gx fish_user_paths $fish_user_paths $fzf_dir/bin
 end
+
+if type -q n
+    set -gx N_PREFIX $HOME/.n
+    set -gx fish_user_paths $N_PREFIX/bin $fish_user_paths
+end
